c9d44d84115d07ed326eeb8fec7046bc4d45e0f8
Nate Rudnick-Aldridge <nate.rudnickaldridge@gmail.com>
2022-04-28 19:15:06 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,13 @@ Team.destroy_all
 
 ppp = Team.create(name: "Pat's Pallino Pals", abbreviation: "PPP")
 blt = Team.create(name: "Bocce League Team", abbreviation: "BLT")
-inc = Team.create(name: "Inchworms", abbreviation: "INC")
+wrm = Team.create(name: "wrmhworms", abbreviation: "WRM")
 hot = Team.create(name: "Hot Rollers", abbreviation: "HOT")
+
+# ppp = Team.find_by(abbreviation: "PPP")
+# blt = Team.find_by(abbreviation: "BLT")
+# wrm = Team.find_by(abbreviation: "WRM")
+# hot = Team.find_by(abbreviation: "HOT") 
 
 g = Game.create(venue: "Wyman Park")
 GameScore.create(game: g, team: blt, score: 9, win: false)
@@ -14,11 +19,11 @@ GameScore.create(game: g, team: hot, score: 11, win: true)
 
 g = Game.create(venue: "Wyman Park")
 GameScore.create(game: g, team: ppp, score: 9, win: false)
-GameScore.create(game: g, team: inc, score: 11, win: true)
+GameScore.create(game: g, team: wrm, score: 11, win: true)
 #===================
 g = Game.create(venue: "Wyman Park")
 GameScore.create(game: g, team: hot, score: 2, win: false)
-GameScore.create(game: g, team: inc, score: 15, win: true)
+GameScore.create(game: g, team: wrm, score: 15, win: true)
 
 g = Game.create(venue: "Wyman Park")
 GameScore.create(game: g, team: blt, score: 12, win: false)
@@ -30,4 +35,12 @@ GameScore.create(game: g, team: hot, score: 13, win: true)
 
 g = Game.create(venue: "Wyman Park")
 GameScore.create(game: g, team: blt, score: 6, win: false)
-GameScore.create(game: g, team: inc, score: 15, win: true)
+GameScore.create(game: g, team: wrm, score: 15, win: true)
+#===================
+g = Game.create(venue: "Wyman Park")
+GameScore.create(game: g, team: blt, score: 15, win: true)
+GameScore.create(game: g, team: hot, score: 10, win: false)
+
+g = Game.create(venue: "Wyman Park")
+GameScore.create(game: g, team: ppp, score: 13, win: true)
+GameScore.create(game: g, team: wrm, score: 6, win: false)
